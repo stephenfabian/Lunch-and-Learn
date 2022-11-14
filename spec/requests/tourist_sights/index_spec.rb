57b43af7afd24c8b6932327d3ee27a6963c36d6a
Stephen Fabian <fabianstephen@gmail.com>
2022-11-14 10:58:24 -0700

    happy path tests for tourist sight index

---
+++ spec/requests/tourist_sights/index_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+RSpec.describe 'Tourist Sights Search', :vcr do
+  it 'Happy path - search with valid country returns collection of all tourist sights within 20000 meter radius of the capital city' do
+    country_params = {country: "New Zealand"}
+    get api_v1_tourist_sights_path, headers: headers, params: country_params
+    parsed_response = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+    parsed_response[:data].each do |sight|
+      structural_tests(sight)
+    end
+  end
+
+  it 'Happy path (second country) - search with valid country returns collection of all tourist sights within 20000 meter radius of the capital city' do
+    country_params = {country: "France"}
+    get api_v1_tourist_sights_path, headers: headers, params: country_params
+    parsed_response = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+    parsed_response[:data].each do |sight|
+      structural_tests(sight)
+    end
+  end
+
+  it 'Happy path(third_country) - search with valid country returns collection of all tourist sights within 20000 meter radius of the capital city' do
+    country_params = {country: "Latvia"}
+    get api_v1_tourist_sights_path, headers: headers, params: country_params
+    parsed_response = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+    parsed_response[:data].each do |sight|
+      structural_tests(sight)
+    end
+  end
+
+  def structural_tests(sight)
+     expect(sight).to have_key(:id)
+    expect(sight[:type]).to eq("tourist_sight")  
+    expect(sight[:attributes]).to have_key(:name)
+    expect(sight[:attributes]).to have_key(:address)
+    expect(sight[:attributes]).to have_key(:place_id)
+  end
+end
